25a935d0301341f04749c224aa62d0b3b0310a90
rohannes220 <rohankumar.id@gmail.com>
2023-04-16 22:51:06 -0700

    Add test class with main method and for loop

---
+++ code/src/practice/Test.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("rohan");
+        }
+    }
+}
