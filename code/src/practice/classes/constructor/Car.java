56d84a2c5aafc00108ad1b9e083fdc9a43ea5bc3
rohannes220 <rohankumar.id@gmail.com>
2023-04-30 21:54:54 -0700

    create car class and constructor

---
+++ code/src/practice/classes/constructor/Car.java
@@ -0,0 +1,17 @@
+package practice.classes.constructor;
+
+public class Car {
+    int year;
+    double mileage;
+    String make;
+    String model;
+    String color;
+
+    Car(String ma, String mo, int y, double m, String c){
+        year = y;
+        mileage = m;
+        make = ma;
+        model = mo;
+        color = c; 
+    }
+}
