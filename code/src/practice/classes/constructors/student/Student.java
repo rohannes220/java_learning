385c25203ace823b3361a132f17db1cbd054306e
rohannes220 <rohankumar.id@gmail.com>
2023-04-30 22:28:53 -0700

    create student classes

---
+++ code/src/practice/classes/constructors/student/Student.java
@@ -0,0 +1,14 @@
+package practice.classes.constructors.student;
+
+public class Student {
+    String name;
+    double grade;
+    boolean isMarried;
+
+    Student(String n, double g) {
+        name = n;
+        grade = g;
+        isMarried = false;
+    }
+
+}
