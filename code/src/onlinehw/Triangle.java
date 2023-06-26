d5b85b4cec98492350723c12b87112042e56d3fa
rohannes220 <rohankumar.id@gmail.com>
2023-06-25 23:06:56 -0700

    Add feature to type triangles

--- code/src/onlinehw/Triangle.java
+++ code/src/onlinehw/Triangle.java
@@ -11,35 +11,89 @@ public class Triangle {
         }
     }
 
+    public static boolean isEquilateral(int a, int b, int c) {
+        if (a == b && b == c && a == c) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isIsoceles(int a, int b, int c) {
+        if (a == b || b == c || c == a) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isScalene(int a, int b, int c) {
+        if (isEquilateral(a, b, c) == false && isIsoceles(a, b, c) == false) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isRight(int a, int b, int c) {
+        if ((a * a) + (b * b) == (c * c) || (a * a) + (c * c) == (b * b) || (b * b) + (c * c) == (a * a)) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        // Enter first side: 3
-        // Enter second side: 4
-        // Enter third side: 5
-        // (3, 4, 5) form a triangle
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first side: ");
+        int x = s.nextInt();
+
+        System.out.print("Enter second side: ");
+        int y = s.nextInt();
+
+        System.out.print("Enter third side: ");
+        int z = s.nextInt();
+
+        if (isTriangle(x, y, z) == false) {
+            // if it is not a triangle
+            System.out.println("(" + x + ", " + y + ", " + z + ") don't form a triangle");
+        } else if (isEquilateral(x, y, z) == true) {
+            // if it is a triangle and equliateral
+            System.out.println("(" + x + ", " + y + ", " + z + ") form an equilateral triangle");
+        } else if (isIsoceles(x, y, z) == true) {
+            // if it is a triangle and not equilateral and is isoceles
+            System.out.println("(" + x + ", " + y + ", " + z + ") form an isoceles triangle");
+        } else if (isRight(x, y, z) == true) {
+            // if it is a triangle and not (equilateral isoceles) and is right
+            System.out.println("(" + x + ", " + y + ", " + z + ") form a right triangle");
+        } else if (isScalene(x, y, z) == true) {
+            // if it is a triangle and not (equilateral, iscoleles, and scalene) but is a
+            // scalene triangle
+            System.out.println("(" + x + ", " + y + ", " + z + ") form a scalene triangle");
+        }
+
+        s.close();
 
         // Enter first side: 13
         // Enter second side: 4
         // Enter third side: 5
-        // (13, 4, 5) doesn't form a triangle
+        // (13, 4, 5) don't form a triangle
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter first side: ");
-        int firstSide = s.nextInt();
+        // Enter first side: 3
+        // Enter second side: 4
+        // Enter third side: 5
+        // (3, 4, 5) form a right angle triangle
 
-        System.out.print("Enter second side: ");
-        int secondSide = s.nextInt();
+        // Enter first side: 3
+        // Enter second side: 3
+        // Enter third side: 3
+        // (3, 3, 3) form an equilateral triangle
 
-        System.out.print("Enter third side: ");
-        int thirdSide = s.nextInt();
+        // Enter first side: 2
+        // Enter second side: 100
+        // Enter third side: 100
+        // (2, 100, 100) form a isoceles triangle
 
-        boolean result = isTriangle(firstSide, thirdSide, secondSide);
-
-        if (result == true) {
-            System.out.println("(" + firstSide + ", " + secondSide + ", " + thirdSide + ")" + " form a triangle");
-        } else {
-            System.out.println("(" + firstSide + ", " + secondSide + ", " + thirdSide + ")" + " doesn't form a triangle");
-        }
-
-        s.close();
+        // Enter first side: 2
+        // Enter second side: 5
+        // Enter third side: 9
+        // (2, 100, 100) form a scalene triangle
     }
 }
