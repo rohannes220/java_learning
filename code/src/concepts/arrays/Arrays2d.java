1b4f14bb4d34bc8c30b76f9232da8b80b35b44d4
rohannes220 <rohankumar.id@gmail.com>
2023-07-10 22:05:05 -0700

    put array on multiple lines

--- code/src/concepts/arrays/Arrays2d.java
+++ code/src/concepts/arrays/Arrays2d.java
@@ -2,7 +2,11 @@ package concepts.arrays;
 
 public class Arrays2d {
     public static void main(String[] args) {
-        int[][] arr = { { 1, 2, 3, 11, 12 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] arr = new int[][] {
+                { 1, 2, 3, 11, 12 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
         System.out.println(arr[0][1]); // 2
         System.out.println(arr[0][3]); // 11
 
