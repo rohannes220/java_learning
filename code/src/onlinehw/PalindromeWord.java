9fbf36d4b50989ab910f87e4d7ad208f8e428eef
rohannes220 <rohankumar.id@gmail.com>
2023-07-13 22:52:47 -0700

    create PalindromeWord.java

---
+++ code/src/onlinehw/PalindromeWord.java
@@ -0,0 +1,87 @@
+package onlinehw;
+
+public class PalindromeWord {
+
+    public static boolean isPalindrome(String s) {
+        int len = s.length();
+        for (int f = 0; f < (len / 2); f++) {
+            char fwd = s.charAt(f);
+            char bkwd = s.charAt(len - f - 1);
+            if (fwd != bkwd) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void testIsPalindrome() {
+        // Madam -> false
+        // mAdAm -> true
+        // DAD -> true
+        // Rohan -> false
+        // "" -> true
+        // a -> true
+        // Ab -> false
+        // AAb -> false
+        // LL -> true
+        // oL -> false
+        // uxcb -> false
+        // lol -> true
+        // acca -> true
+        // zzzzzzz -> true
+        String[] inputs = new String[] {
+                "Madam",
+                "mAdAm",
+                "DAD",
+                "Rohan",
+                "",
+                "a",
+                "Ab",
+                "AAb",
+                "LL",
+                "oL",
+                "uxcb",
+                "lol",
+                "acca",
+                "zzzzzzz",
+        };
+        boolean[] expectedOutputs = new boolean[] {
+                false,
+                true,
+                true,
+                false,
+                true,
+                true,
+                false,
+                false,
+                true,
+                false,
+                false,
+                true,
+                true,
+                true,
+        };
+
+        int totalTestCases = inputs.length;
+        int failedCases = 0;
+        for (int i = 0; i < totalTestCases; i++) {
+            String input = inputs[i];
+            boolean expectedOutput = expectedOutputs[i];
+            boolean actualOutput = isPalindrome(input);
+            if (expectedOutput != actualOutput) {
+                System.out.println("Failed Test Case: ");
+                System.out.println("Input: " + input);
+                System.out.println("Expected Output: " + expectedOutput);
+                System.out.println("Actual Output: " + actualOutput);
+                failedCases += 1;
+                System.out.println();
+            }
+        }
+        System.out.println("Testing of isPalindrome Complete!");
+        System.out.println("Success rate: " + (totalTestCases - failedCases) + "/" + totalTestCases);
+    }
+
+    public static void main(String[] args) {
+        testIsPalindrome();
+    }
+}
